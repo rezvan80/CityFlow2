ca7a730dcb886ef6fd2ff83fa8cbd9fb34fcf898
rezvan80 <rbeheshtinezhad80@gmail.com>
2025-02-27 04:51:27 +0330

    Update c2.py

--- c2.py
+++ c2.py
@@ -278,33 +278,90 @@ import torch
 import torch.nn as nn
 from torch.nn import Module
 from sklearn.metrics import accuracy_score
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 # Sum the number of elements in all parameters (weights) of the model
 total_params = sum(p.numel() for p in student_model.policy.parameters())
 model=PPO.load("1740345039")
+model1=PPO.load("1740345039")
+X = torch.randint(0, 20, (10,8,8))
+print(model.policy.forward(torch.tensor(X).float(),deterministic=True))
+print(model1.policy.forward(torch.tensor(X).float(),deterministic=True))
+
 print(model.policy)
 print(f"Total number of weights in the model: {total_params}")
 print(type(student_model.policy.parameters()))
-optimizer = torch.optim.Adam(student_model.policy.parameters(), lr=0.3)
-for i in range(1,2000):
-  observation = 30*np.random.rand(1, 64).tolist()
+optimizer = torch.optim.Adam(model1.policy.parameters(), lr=0.03)
+mse_loss = nn.MSELoss()
+
+criterion = nn.MSELoss()
+epochs = 10000
+num_samples = 1000
+input_size = (3, 32, 32)  # Example image size
+num_classes = 10
+
+X = torch.randint(0, 20, (10,8,8))
+with torch.no_grad():
+  y1 = torch.squeeze(model.policy.forward(torch.tensor(X).float(),deterministic=True)[1])
+  y2 = torch.squeeze(model.policy.forward(torch.tensor(X).float(),deterministic=True)[2])
+
+
+
+dataset = TensorDataset(X, y1.detach(), y2.detach())
+dataloader = DataLoader(dataset, batch_size=20, shuffle=True)
+
+print(dataset)
+for param in model.policy.parameters():
+    param.requires_grad = False
+for epoch in range(epochs):
+  t= 0
+  dataloader = DataLoader(dataset, batch_size=20, shuffle=True)
+  for batch_inputs, batch_labels1, batch_labels2 in dataloader:
+    #  train_dataset = TensorDataset(student_action_logits2,observation)
+    #  train_dataloader = DataLoader(train_dataset, batch_size=20, shuffle=True)
+    #  for batch_idx, (data, target) in enumerate(train_dataloader):
+        # Zero the gradients
+    optimizer.zero_grad()
+
+        #student_action_logits = student_model.policy.forward(torch.tensor(data).float())
+        # Forward pass
+    student_action_logits =model1.policy.forward(torch.tensor(batch_inputs).float(),deterministic=True)
+    #print("student_action_logits requires_grad:", student_action_logits[1].requires_grad)
+    #print("batch_labels requires_grad:", batch_labels1.requires_grad)
+    loss = criterion(student_action_logits[1],batch_labels1)+criterion(student_action_logits[2],batch_labels2)
+    #print(f"loss:{loss}")
+    print(batch_labels1,student_action_logits[1],student_action_logits[2],batch_labels2,loss)
+        # Backward and optimize
+    t=t+loss
+    loss.backward()
+    
+    optimizer.step()
+
+ #  if (epoch + 1) % 10 == 0:
+  #print(f"Epoch [{epoch+1}/{epochs}], Loss: {t.item():.4f}")
+for i in range(1,200000):
+  observation = np.random.randint(1, 30 ,(20,8,8))
   #print(observation)
-  student_action_logits = student_model.policy.forward(torch.tensor(observation).float())
-  student_action_logits2 = model.policy.forward(torch.tensor(observation).float())
-  #print(student_action_logits)
+  for param in model.policy.parameters():
+    param.requires_grad = False
+  student_action_logits =student_model.policy.forward(torch.tensor(observation).float())
+  sudent_action_logits2 =model.policy.forward(torch.tensor(observation).float())
+  #train_dataloader = DataLoader(student_action_logits2, batch_size=20, shuffle=True)
+  print(student_action_logits)
   #print(student_action_logits2)
   optimizer.zero_grad()
-  accuracy=accuracy_score(student_action_logits[0] , student_action_logits2[0])
+  #accuracy=accuracy_score(student_action_logits[0] , student_action_logits2[0])
   #loss=[0,0]
   #loss[0]=student_action_logits2[1]-student_action_logits[1]
   #loss[1]=student_action_logits2[2]-student_action_logits[2]
-  loss1 = torch.nn.functional.mse_loss(student_action_logits[1]  , student_action_logits2[1])
+  loss1 = mse_loss(student_action_logits[1]  , student_action_logits2[1])
   #print(f"Loss at step {i}: {loss.item()}")
-  loss2 = torch.nn.functional.mse_loss(student_action_logits[2] , student_action_logits2[2])
+  loss2 = mse_loss(student_action_logits[2] , student_action_logits2[2])
   #loss1.backward()
   loss2.backward()
   optimizer.step()
-  print(f"Iteration {i}: Loss1 = {loss1.item():.4f},Loss2 = {loss2.item():.4f}, Accuracy = {accuracy:.2f}%")
-  for name,param in student_model.policy.named_parameters():
+  print(f"Iteration {i}: Loss1 = {loss1:.4f},Loss2 = {loss2.item():.4f}")
+  for name,param in model.policy.named_parameters():
         if param.grad is not None:
             #print(name)
             if name == 'mlp_extractor.policy_net.2.weight':
